8f77362136c5f42c980658bc6e7ad0c7fa10b0ac
PENCIL-TT <hariharantt015@gmail.com>
2025-12-20 19:16:41 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,19 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
 import Services from "./pages/Services";
+
+// Services pages
 import FCL from "./pages/services/FCL";
 import LCL from "./pages/services/LCL";
 import ThreePL from "./pages/services/ThreePL";
 import ProjectCargo from "./pages/services/ProjectCargo";
 import AirFreight from "./pages/services/AirFreight";
+import LiquidTransportation from "./pages/services/LiquidTransportation";
+import Warehousing from "./pages/services/Warehousing";
 
 const queryClient = new QueryClient();
 
@@ -21,15 +26,26 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-           <Route path="/services" element={<Services />} />
+          {/* Home */}
           <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
+
+          {/* Services main page */}
+          <Route path="/services" element={<Services />} />
+
+          {/* Services detail pages */}
           <Route path="/services/fcl" element={<FCL />} />
-            <Route path="/services/lcl" element={<LCL />} />
-            <Route path="/services/3pl" element={<ThreePL />} />
-           <Route path="/services/project-cargo" element={<ProjectLogistics />} />
+          <Route path="/services/lcl" element={<LCL />} />
+          <Route path="/services/3pl" element={<ThreePL />} />
+          <Route path="/services/project-cargo" element={<ProjectCargo />} />
           <Route path="/services/air-freight" element={<AirFreight />} />
+          <Route
+            path="/services/liquid-transportation"
+            element={<LiquidTransportation />}
+          />
+          <Route path="/services/warehousing" element={<Warehousing />} />
+
+          {/* 404 – always LAST */}
+          <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
