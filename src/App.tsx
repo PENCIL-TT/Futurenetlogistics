d3826d8c5aa558e8c7afe934b8dd56790da523ca
PENCIL-TT <hariharantt015@gmail.com>
2025-12-21 14:30:33 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,9 @@ import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-// Pages
 import Index from "./pages/Index";
-import Services from "./pages/Services";
-import ThreePL from "./pages/3PL";
-import AirFreight from "./pages/AirFreight";
-import Fcl from "./pages/Fcl";
-import Lcl from "./pages/Lcl";
-import LiquidTransportation from "./pages/LiquidTransportation";
-import ProjectCargo from "./pages/ProjectCargo";
-import Warehousing from "./pages/Warehousing";
 import NotFound from "./pages/NotFound";
+import Services from "./pages/Services";
 
 const queryClient = new QueryClient();
 
@@ -25,31 +16,9 @@ const App = () => (
       <Sonner />
       <BrowserRouter>
         <Routes>
-          {/* Home */}
+           <Route path="/services" element={<Services />} />
           <Route path="/" element={<Index />} />
-
-          {/* Services Main */}
-          <Route path="/services" element={<Services />} />
-
-          {/* Individual Services */}
-          <Route path="/services/3pl" element={<ThreePL />} />
-          <Route path="/services/air-freight" element={<AirFreight />} />
-          <Route path="/services/fcl" element={<Fcl />} />
-          <Route path="/services/lcl" element={<Lcl />} />
-          <Route
-            path="/services/liquid-transportation"
-            element={<LiquidTransportation />}
-          />
-          <Route
-            path="/services/project-cargo"
-            element={<ProjectCargo />}
-          />
-          <Route
-            path="/services/warehousing"
-            element={<Warehousing />}
-          />
-
-          {/* 404 Page */}
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
