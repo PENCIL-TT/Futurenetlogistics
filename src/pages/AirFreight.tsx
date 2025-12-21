a9ca3c2ce8d19ffc8fdc06c0077157ba44ffc50b
PENCIL-TT <hariharantt015@gmail.com>
2025-12-21 14:30:57 +0530

    Update AirFreight.tsx

--- src/pages/AirFreight.tsx
+++ src/pages/AirFreight.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Header } from "@/components/layout/Header";
-import { Footer } from "@/components/layout/Footer";
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 import { motion } from 'framer-motion';
 import { Plane, Clock, Globe, Headset } from "lucide-react";
 import { Link } from 'react-router-dom';
