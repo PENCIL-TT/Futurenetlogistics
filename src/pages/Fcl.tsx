4fdcf7877f4a9383d118fd798b09ebe010d00d5f
PENCIL-TT <hariharantt015@gmail.com>
2025-12-23 11:25:42 +0530

    Update Fcl.tsx

--- src/pages/Fcl.tsx
+++ src/pages/Fcl.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Header } from "@/components/layout/Header";
-import { Footer } from "@/components/layout/Footer";
+import { Header } from "@/components/Header";
+import { Footer } from "@/components/Footer";
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom';
 import { AspectRatio } from "@/components/ui/aspect-ratio";
